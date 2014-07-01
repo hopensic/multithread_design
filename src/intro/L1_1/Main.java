14877a71819bece678e8ecad93d7c3125c89bc54
hopensic <hopensic@gmail>
2014-07-02 00:14:14 +0800

    test

---
+++ src/intro/L1_1/Main.java
@@ -0,0 +1,13 @@
+package intro.L1_1;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for (int i = 0; i < 100; i++)
+			System.out.print("Good!");
+	}
+
+}
